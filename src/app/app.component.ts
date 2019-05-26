46f3f9398470844b534402757e5c47c7886df598
shobhasnkote <50224447+shobhasnkote@users.noreply.github.com>
2019-05-26 23:29:15 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import {Employes } from './employes';
 import { Observable } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Pipe({
+  name: 'sortBy'
+})
+
+export class AppComponent{
 
   found:boolean = false;
+
+  apiUrl = 'http://ergast.com/api/f1/drivers.json?limit=10&offset=20';
+  localApiUrl = 'http://localhost:8080/emp/';
  
   private empData: any = [];
 
@@ -21,11 +29,11 @@ export class AppComponent {
   }
 
   getProfile(){
-    this.httpClient.get('http://ergast.com/api/f1/drivers.json?limit=10&offset=20')
+    this.httpClient.get(this.localApiUrl)
     .subscribe(data => {
       this.empData = data;
       console.log(this.empData);
     })
   }
- 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
