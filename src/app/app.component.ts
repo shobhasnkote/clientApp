8ad83ce5d3b835253f991b83d618701daa6a7372
shobhasnkote <50224447+shobhasnkote@users.noreply.github.com>
2019-05-29 21:37:59 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
-import {Employes } from './employes';
-import { Observable } from 'rxjs';
-import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-@Pipe({
-  name: 'sortBy'
 })
 
 export class AppComponent{
@@ -22,6 +16,8 @@ export class AppComponent{
  
   private empData: any = [];
 
+  private empData1: any = [];
+
   constructor(private httpClient : HttpClient){
   }
   onNameKeyUp(event:any){
@@ -29,11 +25,14 @@ export class AppComponent{
   }
 
   getProfile(){
-    this.httpClient.get(this.localApiUrl)
+    this.httpClient.get(this.apiUrl)
     .subscribe(data => {
       this.empData = data;
-      console.log(this.empData);
+
+      this.empData1 = this.empData.MRData.DriverTable.Drivers
+      // // console.log(this.empData.MRData.drivers);
+        console.log(this.empData.MRData.DriverTable.Drivers)
     })
-  }
+  } 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NewComponent } from './new/new.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent 
+    AppComponent,
+    NewComponent 
     ],
   imports: [
     BrowserModule,
